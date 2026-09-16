cedf7644929868af2581e59f6a89b837b1f4cea2
Far-Se <cargo.sergiu@gmail.com>
2026-09-16 08:38:10 +0300

    Add Winamp2 quick menu design

--- lib/pages/quickmenu_designs/designs.dart
+++ lib/pages/quickmenu_designs/designs.dart
@@ -41,6 +41,7 @@ import 'design_terminal2.dart';
 import 'design_tui.dart';
 import 'design_vector.dart';
 import 'design_winamp.dart';
+import 'design_winamp2.dart';
 import 'design_windows_98.dart';
 import 'design_windows_xp.dart';
 
@@ -89,6 +90,13 @@ class _LoadQuickMenuDesignState extends State<LoadQuickMenuDesign> with QuickMen
       await windowManager.setMinimumSize(Size(controlPanelWidth, Globals.quickMenuSize.height));
       if (size.width < controlPanelWidth) {
         await windowManager.setSize(Size(controlPanelWidth, size.height));
+      }
+    } else if (design == QuickMenuDesigns.winamp2) {
+      const double winamp2Width = 550;
+      final Size size = await windowManager.getSize();
+      await windowManager.setMinimumSize(Size(winamp2Width, Globals.quickMenuSize.height));
+      if (size.width < winamp2Width) {
+        await windowManager.setSize(Size(winamp2Width, size.height));
       }
     } else {
       await windowManager.setMinimumSize(Size(Globals.quickMenuSize.width, Globals.quickMenuSize.height));
@@ -150,6 +158,7 @@ class _LoadQuickMenuDesignState extends State<LoadQuickMenuDesign> with QuickMen
       QuickMenuDesigns.commandDeck => MainMenuCommandDeckWidget(key: ValueKey<int>(_refreshCounter)),
       QuickMenuDesigns.switchboard => MainMenuSwitchboardWidget(key: ValueKey<int>(_refreshCounter)),
       QuickMenuDesigns.orbit => MainMenuOrbitWidget(key: ValueKey<int>(_refreshCounter)),
+      QuickMenuDesigns.winamp2 => MainMenuWinamp2Widget(key: ValueKey<int>(_refreshCounter)),
       // QuickMenuDesigns.familyGuy => MainMenuFamilyGuyWidget(key: ValueKey<int>(_refreshCounter)),
     };
   }
