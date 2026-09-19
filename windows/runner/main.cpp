0b63eae68aca7add60a4fa928a00a1b700b867cc
Far-Se <cargo.sergiu@gmail.com>
2026-09-19 10:01:42 +0300

    Disable Impeller on Windows runner

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -18,6 +18,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
   flutter::DartProject project(L"data");
+  project.set_impeller_switch(flutter::ImpellerSwitch::Disabled);
 
   std::vector<std::string> command_line_arguments = GetCommandLineArguments();
 
